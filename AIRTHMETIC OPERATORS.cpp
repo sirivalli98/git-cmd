bf3ea34cffb67f7f886c93958cad9a0c8ed0b511
sirivalli98 <sirivallikolisetti@gmail.com>
2024-12-06 13:37:57 +0530

    Add files via upload

---
+++ AIRTHMETIC OPERATORS.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=5;
+	printf("operation\t result\n");
+	printf("a+b=\t%d",a+b);
+	printf("a-b=\t%d",a-b);
+	printf("a*b=\t%d",a*b);
+	printf("a/b=\t%d",a/b);
+	printf("a%%b=\t%d",a%b);
+	return 0;
+    
+}
